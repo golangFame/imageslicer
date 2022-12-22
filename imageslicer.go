4888948702d04a1394a9b4665ec3a0ac4540af6f
Hiro <laciferin@gmail.com>
2022-12-22 20:31:00 +0530

    :sparkles: Slice

--- imageslicer.go
+++ imageslicer.go
@@ -1,7 +1,33 @@
 package imageslicer
 
-import "image"
+import (
+	"image"
+	"math"
+)
 
-func Slice(img image.Image) (tiles []image.Image) {
+func Slice(img image.Image, grid [2]int) (tiles []image.Image) {
+
+	tiles = make([]image.Image, 0, grid[0]*grid[1])
+
+	shape := img.Bounds()
+
+	widthF := float64(shape.Max.X / grid[1])
+	heightF := float64(shape.Max.Y / grid[0])
+
+	width := int(math.Ceil(widthF))
+	height := int(math.Ceil(heightF))
+
+	for y := shape.Min.Y; y+height <= shape.Max.Y; y += height {
+
+		for x := shape.Min.X; x+width <= shape.Max.X; x += width {
+
+			tile := img.(interface {
+				SubImage(r image.Rectangle) image.Image
+			}).SubImage(image.Rect(x, y, x+width, y+height))
+
+			tiles = append(tiles, tile)
+		}
+	}
+
 	return
 }
