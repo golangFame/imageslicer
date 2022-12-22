0606c196158496952af18a2f3296b6b828d5bc6d
Hiro <laciferin@gmail.com>
2022-12-22 20:57:10 +0530

    :construction: Grid

--- imageslicer.go
+++ imageslicer.go
@@ -5,17 +5,22 @@ import (
 	"math"
 )
 
+type Grid struct {
+	Rows    int
+	Columns int
+}
+
 func Slice(img image.Image, grid [2]int) (tiles []image.Image) {
 
 	tiles = make([]image.Image, 0, grid[0]*grid[1])
 
 	shape := img.Bounds()
 
-	widthF := float64(shape.Max.X / grid[1])
 	heightF := float64(shape.Max.Y / grid[0])
+	widthF := float64(shape.Max.X / grid[1])
 
-	width := int(math.Ceil(widthF))
 	height := int(math.Ceil(heightF))
+	width := int(math.Ceil(widthF))
 
 	for y := shape.Min.Y; y+height <= shape.Max.Y; y += height {
 
