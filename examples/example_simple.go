1258b24deb9460cc7f88429ee5ace4a87e3b20f0
Hiro <goferHiro@gmail.com>
2022-12-24 03:34:09 +0530

    :memo: README.md
    Signed-off-by: Hiro <goferHiro@gmail.com>

---
+++ examples/example_simple.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"github.com/goferHiro/imageslicer"
+	"log"
+)
+
+func main() {
+	imgUrl := "https://static.wikia.nocookie.net/big-hero-6-fanon/images/0/0f/Hiro.jpg/revision/latest?cb=20180511180437"
+
+	img := imageslicer.GetImageFromUrl(imgUrl)
+
+	if img == nil {
+		log.Fatalln("invalid image url or image format not supported!")
+	}
+
+	grid := [2]uint{2, 2} //rows,columns
+
+	tiles := imageslicer.Slice(img, grid)
+
+	expectedTiles := int(grid[0] * grid[1])
+
+	if len(tiles) != expectedTiles {
+		log.Fatalf("expected %d rcvd %d\n", expectedTiles, len(tiles))
+	}
+
+	//lets join the tiles back
+
+	joinedImg, err := imageslicer.Join(tiles, grid)
+
+	if err != nil {
+		log.Fatalf("joining tiles failed due to %s", err)
+	}
+
+	shapeJ := joinedImg.Bounds()
+	shapeI := img.Bounds()
+
+	fmt.Println(shapeJ, shapeI) //shape might change due to pixel loss
+
+}
