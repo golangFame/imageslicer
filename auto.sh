398a0933cdc885598190ea93359e44742eafdb47
Hiro <goferHiro@gmail.com>
2022-12-25 02:28:44 +0530

    :bug: auto.sh
    Signed-off-by: Hiro <goferHiro@gmail.com>

--- auto.sh
+++ auto.sh
@@ -14,9 +14,11 @@ tag=$(echo "$tag" | sed 's/ //g')
 
 export TAG=$tag
 
+export go=../linux/go/bin/go # is the 19 version
 
 chmod +x scripts/release.sh
 chmod +x scripts/tag.sh
+
 
 ./scripts/release.sh
 ./scripts/tag.sh
